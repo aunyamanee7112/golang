910f2e5ef06ff6376183172d4828872c5a0f8f5b
Home <home@Homes-MacBook-Pro.local>
2022-10-17 11:51:01 +0700

    feat: 🎸 variable & type

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(("hello first time golang"))
+	// variable can declare with type and shorthand
+	// string
+	name := "aun"
+	var name1 = "apunn"
+	var name2 string = "pun"
+	// number
+	num := 1
+	var num1 int = 2
+	var num2 float32 = 1.3
+	// bit and memory
+	var value uint8 = 255
+	var value1 uint16 = 30000
+	fmt.Println(name, name1, name2, num, num1, num2, value, value1)
 }
